7dc60990aa8392f93b93b8f54deda31dd6716beb
Pradeep7kumar <pradeepahwr@gmail.com>
2020-01-13 23:49:04 +0530

    Updates

---
+++ precache-manifest.2dd08f9788297f61ed27a6ab828230dc.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d35d1596113c9726dc901f3768cd22e5",
+    "url": "/Robo-Friends/index.html"
+  },
+  {
+    "revision": "7c9dbeef573817c143e9",
+    "url": "/Robo-Friends/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "303afeed0b5e0106affa",
+    "url": "/Robo-Friends/static/css/main.783cc2fb.chunk.css"
+  },
+  {
+    "revision": "7c9dbeef573817c143e9",
+    "url": "/Robo-Friends/static/js/2.3a77dedc.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/Robo-Friends/static/js/2.3a77dedc.chunk.js.LICENSE"
+  },
+  {
+    "revision": "303afeed0b5e0106affa",
+    "url": "/Robo-Friends/static/js/main.f319bf40.chunk.js"
+  },
+  {
+    "revision": "1f6968f1cddc5eb40d16",
+    "url": "/Robo-Friends/static/js/runtime-main.6da221f7.js"
+  }
+]);
